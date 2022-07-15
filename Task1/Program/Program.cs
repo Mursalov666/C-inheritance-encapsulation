1dd22b23a09cdb6a10beb5f25a82ea132bd1489d
Ragif <rafigrm@code.edu.az>
2022-07-15 12:41:18 +0400

    Task1

---
+++ Task1/Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Program
+{
+
+    public class Program
+    {
+        static void Main()
+        {
+            DocumentProgram basic = new DocumentProgram();
+            ExpertDocumentProgram expert = new ExpertDocumentProgram();
+            ProDocumentProgram pro = new ProDocumentProgram();
+
+            basic.OpenDocument();
+            basic.EditDocument();
+            basic.SaveDocument();
+        }
+
+
+    }
+
+}
